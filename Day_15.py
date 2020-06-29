cc449a319aa4487240f1ab0ef5ddcd43b0b96e49
Sumanpal3108 <64361664+Sumanpal3108@users.noreply.github.com>
2020-06-29 14:11:51 +0530

    #Day_15
    Easter Egg:- DECLARE VARIABLES NOT WAR

---
+++ Day_15.py
@@ -0,0 +1,13 @@
+﻿str2 = input("Give your message here: ")
+s = 0
+for i in str2:
+    if i == " ":
+        print (chr(ord(i)),end = "" )
+ 
+    elif s%2 == 0:
+        print (chr(ord(i)-1),end = "" )
+        s+=1
+    elif s%2 == 1:
+        print (chr(ord(i)+1),end = "" )
+        s+=1
+input("Enter any key to exit")
